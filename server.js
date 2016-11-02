3648b15ba22cd6d833e1716da2ec879342279fe2
Srajan Soni <sonisrajan96@gmail.com>
2016-11-02 15:23:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,9 +141,25 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    pool.query("select *from article where title='"+req.params.articleName+"';",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+        
+            if(result.rows.length===0)
+            re.status(404).send("Article not found");
+            else{
+            var articleData=result.rows[0];
+                  res.send(createTemplate([articleData]));
+            }
+            
+        }
+        
+    })
+
 });
 
 app.get('/article-two', function (req, res) {
