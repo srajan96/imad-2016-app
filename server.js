cdc0a115a775c50586e0890707f177fbf144524d
Srajan Soni <sonisrajan96@gmail.com>
2016-11-02 15:27:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,6 +153,7 @@ app.get('/articles/:articleName', function (req, res) {
             re.status(404).send("Article not found");
             else{
             var articleData=result.rows[0];
+            console.log(articleData);
                   res.send(createTemplate([articleData]));
             }
             
